49afdb14645df49bb1e5a8d2bcaaa4dce0e01399
koseonje <kosunje1344@naver.com>
2024-08-02 02:21:07 +0900

    feat : 글로벌 예외 처리 세팅

--- src/main/java/com/example/rentalSystem/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rentalSystem/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,21 @@
 package com.example.rentalSystem.global.exception;
 
+import com.example.rentalSystem.global.response.ErrorType;
+import com.example.rentalSystem.global.response.ExceptionResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ExceptionResponse handleException(Exception e) {
+    log.warn(e.getMessage(), e);
+    return ExceptionResponse.of(ErrorType.BAD_REQUEST);
+  }
 }
