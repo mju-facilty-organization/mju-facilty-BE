0df2126dcad74fda526eb49c7f67d62633befa77
koseonje <kosunje1344@naver.com>
2024-08-03 17:15:56 +0900

    feat : 테스트 관련 config

---
+++ src/test/java/com/example/rentalSystem/common/config/TestAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.rentalSystem.common.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@TestConfiguration
+@EnableJpaAuditing
+public class TestAuditingConfig {
+}
