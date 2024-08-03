c1585be9d893fcea49d4095a574e560fd0af666e
koseonje <kosunje1344@naver.com>
2024-08-03 22:02:22 +0900

    refactor : Api 응답 타입(Enum)에 httpstatus 속성 추가

--- src/main/java/com/example/rentalSystem/global/response/SuccessType.java
+++ src/main/java/com/example/rentalSystem/global/response/SuccessType.java
@@ -2,13 +2,15 @@ package com.example.rentalSystem.global.response;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum SuccessType {
 
-  SUCCESS(HttpStatusCode.OK, "GLOBAL-001", "[SUCCESS] 요청에 성공하였습니다.");
+  SUCCESS(HttpStatus.OK, HttpStatusCode.OK, "GLOBAL-001", "[SUCCESS] 요청에 성공하였습니다.");
 
+  private final HttpStatus httpStatus;
   private final int httpStatusCode;
   private final String successCode;
   private final String message;
