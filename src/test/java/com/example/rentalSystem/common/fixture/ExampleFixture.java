c9d90020c6cf165a57d598caf0fcae0a12a4db40
koseonje <kosunje1344@naver.com>
2024-08-03 17:16:15 +0900

    feat : 테스트 fixture 임시 클래스 추가

---
+++ src/test/java/com/example/rentalSystem/common/fixture/ExampleFixture.java
@@ -0,0 +1,10 @@
+package com.example.rentalSystem.common.fixture;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ExampleFixture {
+
+}
