e519862dcdb162921c7d6c7b1994f1d0f2458261
koseonje <kosunje1344@naver.com>
2024-08-13 22:11:08 +0900

    feat : DataInitalizer 빈등록

--- src/test/java/com/example/rentalSystem/common/config/TestAuditingConfig.java
+++ src/test/java/com/example/rentalSystem/common/config/TestAuditingConfig.java
@@ -1,9 +1,12 @@
 package com.example.rentalSystem.common.config;
 
+import com.example.rentalSystem.common.support.DataInitializer;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @TestConfiguration
 @EnableJpaAuditing
+@Import(DataInitializer.class)
 public class TestAuditingConfig {
 }
