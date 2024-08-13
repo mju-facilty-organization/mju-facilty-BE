26d2936049ffb6b5a0ae2192f137e551424173c4
koseonje <kosunje1344@naver.com>
2024-08-13 22:10:30 +0900

    test : 삭제 API, 전체 조회 API 테스트 작성

--- src/test/java/com/example/rentalSystem/domain/facility/service/FacilityServiceTest.java
+++ src/test/java/com/example/rentalSystem/domain/facility/service/FacilityServiceTest.java
@@ -10,9 +10,15 @@ import static org.mockito.Mockito.verify;
 import com.example.rentalSystem.common.fixture.FacilityFixture;
 import com.example.rentalSystem.domain.facility.dto.request.CreateFacilityRequestDto;
 import com.example.rentalSystem.domain.facility.dto.request.UpdateFacilityRequestDto;
+import com.example.rentalSystem.domain.facility.dto.response.FacilityResponse;
 import com.example.rentalSystem.domain.facility.entity.Facility;
 import com.example.rentalSystem.domain.facility.implement.FacilityFinder;
+import com.example.rentalSystem.domain.facility.implement.FacilityReader;
+import com.example.rentalSystem.domain.facility.implement.FacilityRemover;
 import com.example.rentalSystem.domain.facility.implement.FacilitySaver;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,6 +37,12 @@ class FacilityServiceTest {
 
   @Mock
   private FacilitySaver facilitySaver;
+
+  @Mock
+  private FacilityReader facilityReader;
+
+  @Mock
+  private FacilityRemover facilityRemover;
 
   @DisplayName("시설 생성된 후 저장이 되었는지 테스트 합니다.")
   @Test
@@ -62,5 +74,35 @@ class FacilityServiceTest {
     verify(facilityFinder, times(1)).findById(originFacilityId);
     assertEquals("수정된 이름", originFacility.getName());
     assertEquals("수정된 위치", originFacility.getLocation());
+  }
+
+  @DisplayName("시설을 전체 조회할 수 있습니다.")
+  @Test
+  void 시설_전체_조회() {
+    // given
+    Facility facility = FacilityFixture.createFacility();
+    Facility facility2 = FacilityFixture.createUpdateFacility();
+    Facility facility3 = FacilityFixture.createFacility();
+
+    given(facilityReader.getAll()).willReturn(new ArrayList<>(List.of(facility, facility2, facility3)));
+    // when
+    List<FacilityResponse> facilities = facilityService.getAll();
+    // then
+    Assertions.assertThat(facilities).hasSize(3);
+    Assertions.assertThat(facilities).extracting("name").containsExactly("강의실", "수정된 강의실", "강의실");
+  }
+
+  @DisplayName("존재하는 시설을 성공적으로 삭제한다.")
+  @Test
+  void 시설_삭제() {
+    // given
+    Long facilityId = 1L;
+    Facility facility = FacilityFixture.createFacility();
+    given(facilityFinder.findById(facilityId)).willReturn(facility);
+    // when
+    facilityService.delete(facilityId);
+    // then
+    verify(facilityFinder).findById(facilityId);
+    verify(facilityRemover).delete(facility);
   }
 }
