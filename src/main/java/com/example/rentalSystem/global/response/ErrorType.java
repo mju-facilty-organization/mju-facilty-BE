c18638cdfabdce312e4e6266b88e4c31b4fff0ec
0702yoon <gkstkddbs99@naver.com>
2024-09-03 16:30:31 +0900

    feat : error 코드 추가

--- src/main/java/com/example/rentalSystem/global/response/ErrorType.java
+++ src/main/java/com/example/rentalSystem/global/response/ErrorType.java
@@ -7,12 +7,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorType {
 
-  BAD_REQUEST(400, "잘못된 요청입니다."),
-  ENTITY_NOT_FOUND(400, "요청한 정보로 엔터티를 찾을 수 없습니다."),
-  FAIL_AUTHENTICATION(401,"아이디와 비밀번호가 틀렸습니다."),
-  EXPIRED_ACCESS_TOKEN(400, "만료된 엑세스 토큰입니다."),
-  EXPIRED_REFRESH_TOKEN(400,"만료된 리프레쉬 토큰입니다.");
+    BAD_REQUEST(400, "잘못된 요청입니다."),
+    ENTITY_NOT_FOUND(400, "요청한 정보로 엔터티를 찾을 수 없습니다."),
+    FAIL_AUTHENTICATION(401, "아이디와 비밀번호가 틀렸습니다."),
+    EXPIRED_ACCESS_TOKEN(400, "만료된 엑세스 토큰입니다."),
+    EXPIRED_REFRESH_TOKEN(400, "만료된 리프레쉬 토큰입니다."),
+    EXIST_MAIL(400, "존재하는 이메일입니다."),
+    FAIL_SEND_EMAIL(400, "이메일 전송에 실패하였습니다."),
+    FAIL_CREATE_AUTH_CODE(400, "인증 코드 생성에 실패하였습니다."),
+    EXPIRED_AUTH_TIME(401, "이메일 인증 코드가 만료되었습니다."),
+    MISMATCH_VERIFIED_CODE(401, "이메일 인증 코드가 일치하지 않습니다.");
 
-  private final int httpStatusCode;
-  private final String message;
+    private final int httpStatusCode;
+    private final String message;
 }
