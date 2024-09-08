9c6046da8ae031af98874bfbd1e975650fbe18fd
0702yoon <gkstkddbs99@naver.com>
2024-09-08 14:28:28 +0900

    refactor : code refactor

--- src/main/java/com/example/rentalSystem/global/auth/AuthService.java
+++ src/main/java/com/example/rentalSystem/global/auth/AuthService.java
@@ -5,6 +5,7 @@ import com.example.rentalSystem.global.exception.custom.CustomException;
 import com.example.rentalSystem.global.response.ErrorType;
 import com.example.rentalSystem.global.auth.jwt.component.JwtTokenProvider;
 import com.example.rentalSystem.global.auth.jwt.entity.JwtToken;
+import java.util.Scanner;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -21,13 +22,12 @@ public class AuthService {
     public JwtToken createAuthenticationToken(String loginId, String password) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
             loginId, password);
-        Authentication authentication;
         try {
-            authentication = authenticationManagerBuilder.getObject()
+            Authentication authentication = authenticationManagerBuilder.getObject()
                 .authenticate(authenticationToken);
+            return jwtTokenProvider.generateToken(authentication);
         } catch (Exception e) {
             throw new CustomException(ErrorType.FAIL_AUTHENTICATION);
         }
-        return jwtTokenProvider.generateToken(authentication);
     }
 }
