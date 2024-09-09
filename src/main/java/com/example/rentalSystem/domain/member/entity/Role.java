865c947f3743e12f5b4032e7ed581b26c92d8eba
0702yoon <gkstkddbs99@naver.com>
2024-09-09 17:33:14 +0900

    refactor : 생성자 어노테이션으로 대체

--- src/main/java/com/example/rentalSystem/domain/member/entity/Role.java
+++ src/main/java/com/example/rentalSystem/domain/member/entity/Role.java
@@ -1,17 +1,15 @@
 package com.example.rentalSystem.domain.member.entity;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum Role {
     STUDENT("ROLE_STUDENT"),
     PROFESSOR("ROLE_STUDENT,ROLE_PROFESSOR"),
     ADMIN("ROLE_STUDENT,ROLE_PROFESSOR,ROLE_ADMIN");
-
+    
     private final String roles;
-
-    Role(String roles) {
-        this.roles = roles;
-    }
 
 }
