cdba4bd69b5d71cdcdaac3be5f61163170c81ac9
0702Yoon <gkstkddbs99@naver.com>
2025-01-16 21:47:13 +0900

    refactor : StudentController에 존재한 메일 관련 api EmailController로 이동

--- src/main/java/com/example/rentalSystem/domain/student/presentation/controller/StudentController.java
+++ src/main/java/com/example/rentalSystem/domain/student/presentation/controller/StudentController.java
@@ -1,8 +1,5 @@
 package com.example.rentalSystem.domain.student.presentation.controller;
 
-import com.example.rentalSystem.domain.email.dto.request.EmailRequest;
-import com.example.rentalSystem.domain.email.dto.response.EmailVerificationResult;
-import com.example.rentalSystem.domain.email.service.EmailFacade;
 import com.example.rentalSystem.domain.member.entity.CustomerDetails;
 import com.example.rentalSystem.domain.student.dto.request.StudentSignInRequest;
 import com.example.rentalSystem.domain.student.dto.request.StudentSignUpRequest;
@@ -11,7 +8,7 @@ import com.example.rentalSystem.domain.student.dto.response.StudentListResponse;
 import com.example.rentalSystem.domain.student.dto.response.StudentSignUpResponse;
 import com.example.rentalSystem.domain.student.dto.response.StudentUpdateResponse;
 import com.example.rentalSystem.domain.student.presentation.api.StudentApi;
-import com.example.rentalSystem.domain.student.service.StudentFacade;
+import com.example.rentalSystem.domain.student.service.StudentService;
 import com.example.rentalSystem.global.response.ApiResponse;
 import com.example.rentalSystem.global.response.SuccessType;
 import com.example.rentalSystem.global.auth.jwt.entity.JwtToken;
@@ -26,13 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController implements StudentApi {
 
-    private final StudentFacade studentFacade;
-    private final EmailFacade emailFacade;
+    private final StudentService studentService;
 
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<StudentSignUpResponse> signUp(
         StudentSignUpRequest signUpDto) {
-        StudentSignUpResponse studentSignUpResponse = studentFacade.studentSignUp(signUpDto);
+        StudentSignUpResponse studentSignUpResponse = studentService.studentSignUp(signUpDto);
         return ApiResponse.success(SuccessType.CREATED, studentSignUpResponse);
     }
 
@@ -40,42 +36,20 @@ public class StudentController implements StudentApi {
     public ApiResponse<JwtToken> signIn(StudentSignInRequest studentSignInRequest) {
         String loginId = studentSignInRequest.loginId();
         String password = studentSignInRequest.password();
-        JwtToken jwtToken = studentFacade.userSignIn(loginId, password);
+        JwtToken jwtToken = studentService.userSignIn(loginId, password);
         return ApiResponse.success(SuccessType.SUCCESS, jwtToken);
     }
 
     @Override
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<StudentListResponse> retrieveAllStudent() {
-        return ApiResponse.success(SuccessType.SUCCESS, studentFacade.retrieveAllStudent());
-    }
-
-    @Override
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponse<?> checkDuplicatedEmail(EmailRequest emailRequest) {
-        emailFacade.checkDuplicatedEmail(emailRequest.email());
-        return ApiResponse.success(SuccessType.SUCCESS);
-    }
-
-    @Override
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponse<?> sendCodeToEmail(EmailRequest emailRequest) {
-        emailFacade.sendCodeToEmail(emailRequest.email());
-        return ApiResponse.success(SuccessType.SUCCESS);
-    }
-
-    @Override
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponse<?> checkCode(EmailRequest emailRequest) {
-        EmailVerificationResult emailVerificationResult = emailFacade.verificationCode(
-            emailRequest.email(), emailRequest.code());
-        return ApiResponse.success(SuccessType.SUCCESS, emailVerificationResult);
+        return ApiResponse.success(SuccessType.SUCCESS, studentService.retrieveAllStudent());
     }
 
     @Override
     public ApiResponse<?> updateStudentInfo(StudentUpdateRequest studentUpdateRequest,
         CustomerDetails customerDetails) {
-        StudentUpdateResponse studentUpdateResponse = studentFacade.updateStudentInfo(
+        StudentUpdateResponse studentUpdateResponse = studentService.updateStudentInfo(
             studentUpdateRequest, customerDetails.getUsername());
         return ApiResponse.success(SuccessType.SUCCESS, studentUpdateResponse);
     }
