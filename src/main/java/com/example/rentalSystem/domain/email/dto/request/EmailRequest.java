2efa98e1d4ff10eb0a0f39a525ab600f888bad31
0702Yoon <gkstkddbs99@naver.com>
2025-01-16 21:46:43 +0900

    refactor : 이메일 인증 코드 저장을 db가 아닌 메모리에 저장하도록 변경

--- src/main/java/com/example/rentalSystem/domain/email/dto/request/EmailRequest.java
+++ src/main/java/com/example/rentalSystem/domain/email/dto/request/EmailRequest.java
@@ -1,5 +1,15 @@
 package com.example.rentalSystem.domain.email.dto.request;
 
-public record EmailRequest(String email, String code) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record EmailRequest(
+    @NotBlank(message = "이메일이 비어있습니다")
+    @Pattern(
+        regexp = "^[A-Za-z0-9._%+-]+@mju\\.ac\\.kr$",
+        message = "명지대 이메일을 사용해야합니다."
+    )
+    String email
+) {
 
 }
