55f3cce9b28377b572986dc2f41dcb006491981a
0702Yoon <gkstkddbs99@naver.com>
2025-01-16 22:01:57 +0900

    test : 학생 회원가입 성공 테스트 케이스 구현

--- src/test/java/com/example/rentalSystem/domain/student/presentation/controller/StudentControllerTest.java
+++ src/test/java/com/example/rentalSystem/domain/student/presentation/controller/StudentControllerTest.java
@@ -11,28 +11,33 @@ import com.example.rentalSystem.common.fixture.MemberFixture;
 import com.example.rentalSystem.domain.student.dto.request.StudentSignUpRequest;
 import com.example.rentalSystem.domain.student.dto.response.StudentListResponse;
 import com.example.rentalSystem.domain.student.dto.response.StudentSignUpResponse;
-import com.example.rentalSystem.domain.student.service.StudentFacade;
+import com.example.rentalSystem.domain.student.presentation.controller.StudentController;
+import com.example.rentalSystem.domain.student.service.StudentService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@WebMvcTest(StudentController.class)
+
 @Slf4j
+@WebMvcTest(controllers = StudentController.class)
+@ExtendWith(SpringExtension.class)
 public class StudentControllerTest {
 
     @MockBean
-    private StudentFacade studentFacade;
+    private StudentService studentService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -40,14 +45,14 @@ public class StudentControllerTest {
     @Autowired
     protected ObjectMapper objectMapper;
 
-    @DisplayName("회원 가입 성공")
     @Test
     @WithMockUser
-    void signUpSuccess() throws Exception {
+    @DisplayName("회원 가입 성공")
+    public void 회원가입_학생_성공() throws Exception {
         //given
         StudentSignUpRequest studentSignUpRequest = MemberFixture.studentSignUpRequest();
         StudentSignUpResponse studentSignUpResponse = MemberFixture.studentSignUpResponse();
-        doReturn(studentSignUpResponse).when(studentFacade)
+        doReturn(studentSignUpResponse).when(studentService)
             .studentSignUp(any(StudentSignUpRequest.class));
 
         // when
@@ -56,17 +61,17 @@ public class StudentControllerTest {
                 .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(toJson(studentSignUpRequest)));
+
         // then
-        resultActions.andExpect(status().isOk())
+        resultActions.andExpect(status().isCreated())
             .andExpect(jsonPath("$.data.studentName", studentSignUpResponse).exists());
-        // <-- 경로 다시 한번 보자.
     }
 
     @DisplayName("전체 학생 목록 조회")
     @Test
     void retrieveAllStudent() throws Exception {
         //given
-        doReturn(MemberFixture.studentListResponse()).when(studentFacade).retrieveAllStudent();
+        doReturn(MemberFixture.studentListResponse()).when(studentService).retrieveAllStudent();
 
         //when
         ResultActions resultActions = mockMvc.perform(
