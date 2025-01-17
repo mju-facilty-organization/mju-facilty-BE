19b13f680d137f800e641e6598d518457ff40d31
0702Yoon <gkstkddbs99@naver.com>
2025-01-17 18:56:23 +0900

    test : @springBootTest 제거

--- src/test/java/com/example/rentalSystem/common/support/ApiTestSupport.java
+++ src/test/java/com/example/rentalSystem/common/support/ApiTestSupport.java
@@ -7,19 +7,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-// 컨트롤러 단 통합테스트용
-@SpringBootTest
-@AutoConfigureMockMvc
-public abstract class ApiTestSupport extends TestContainerSupport {
+// 컨트롤러 단 RestDocs 테스트용
+public abstract class ApiTestSupport {
 
-  @Autowired
-  protected MockMvc mockMvc;
+    @Autowired
+    protected MockMvc mockMvc;
 
-  @Autowired
-  protected ObjectMapper objectMapper;
+    @Autowired
+    protected ObjectMapper objectMapper;
 
-  protected String toJson(Object object) throws JsonProcessingException {
-    return objectMapper.writeValueAsString(object);
-  }
+    protected String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
 
+    protected <T> T formJson(String string, Class<T> tClass) throws JsonProcessingException {
+        return objectMapper.readValue(string, tClass);
+    }
 }
