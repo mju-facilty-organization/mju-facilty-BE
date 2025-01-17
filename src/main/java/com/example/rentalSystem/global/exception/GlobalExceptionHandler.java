da18d493a8a0e31d9ef90ee0f80c1e461f4a30c6
0702Yoon <gkstkddbs99@naver.com>
2025-01-17 19:00:07 +0900

    feat : validation에 대한 예외 메시지 처리 추가

--- src/main/java/com/example/rentalSystem/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rentalSystem/global/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,7 +22,7 @@ public class GlobalExceptionHandler {
         log.warn(e.getMessage(), e);
         return ApiResponse.error(ErrorType.BAD_REQUEST);
     }
-    
+
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiResponse<?> handleEntityNotFoundException(EntityNotFoundException e) {
@@ -34,5 +35,17 @@ public class GlobalExceptionHandler {
         log.error("CustomException", e);
         ApiResponse<?> apiResponse = ApiResponse.error(e.getErrorType());
         return ResponseEntity.status(e.getErrorType().getHttpStatusCode()).body(apiResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(error -> error.getDefaultMessage())
+            .findFirst()
+            .orElse("잘못된 요청입니다.");
+        return ApiResponse.error(ErrorType.INVALID_REQUEST, errorMessage);
     }
 }
