247287b69ad224869e476cd7cce9052cbf7ee888
0702Yoon <gkstkddbs99@naver.com>
2025-01-17 18:57:14 +0900

    test : email 중복 검증, 인증 코드 전송, 검증 test 코드 작성

---
+++ src/test/java/com/example/rentalSystem/common/fixture/EmailFixture.java
@@ -0,0 +1,12 @@
+package com.example.rentalSystem.common.fixture;
+
+import com.example.rentalSystem.domain.email.dto.request.EmailRequest;
+
+public class EmailFixture {
+
+    public static EmailRequest emailRequest() {
+        return new EmailRequest(
+            "gkstkddbs99@mju.ac.kr"
+        );
+    }
+}
