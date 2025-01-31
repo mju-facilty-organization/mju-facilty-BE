ef46be29c5cf9524e3a71732eed9bee8a330358c
0702Yoon <gkstkddbs99@naver.com>
2025-01-31 18:12:05 +0900

    feat : 직렬화 값 지정

--- src/main/java/com/example/rentalSystem/domain/facility/entity/RentalStatus.java
+++ src/main/java/com/example/rentalSystem/domain/facility/entity/RentalStatus.java
@@ -1,5 +1,7 @@
 package com.example.rentalSystem.domain.facility.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +11,10 @@ import lombok.RequiredArgsConstructor;
 public enum RentalStatus {
     AVAILABLE("예약가능"), UNAVAILABLE("이용불가"), RESERVED("예약완료"), WAITING("예약대기"), CURRENT("현재예약");
     final String description;
+
+    @JsonValue
+    public String getDescription() {
+        return description;
+    }
+    
 }
