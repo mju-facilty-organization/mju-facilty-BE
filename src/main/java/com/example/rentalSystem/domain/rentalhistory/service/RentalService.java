9acc8bf9f2962be68910dcaeb434789f07263728
0702Yoon <gkstkddbs99@naver.com>
2025-02-02 23:08:12 +0900

    feat : @AuthenticationPrincipal에서의 CustomerDetails에서 Student 반환하는 메서드 추가

--- src/main/java/com/example/rentalSystem/domain/rentalhistory/service/RentalService.java
+++ src/main/java/com/example/rentalSystem/domain/rentalhistory/service/RentalService.java
@@ -4,9 +4,13 @@ import com.example.rentalSystem.domain.facility.entity.Facility;
 import com.example.rentalSystem.domain.facility.implement.FacilityFinder;
 import com.example.rentalSystem.domain.member.entity.Member;
 import com.example.rentalSystem.domain.rentalhistory.dto.CreateRentalRequest;
+import com.example.rentalSystem.domain.rentalhistory.dto.response.RentalHistoryResponseDto;
 import com.example.rentalSystem.domain.rentalhistory.entity.RentalHistory;
 import com.example.rentalSystem.domain.rentalhistory.implement.RentalScheduler;
 import com.example.rentalSystem.domain.rentalhistory.repository.RentalHistoryRepository;
+import com.example.rentalSystem.domain.student.entity.Student;
+import com.example.rentalSystem.domain.student.implement.StudentFinder;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,9 +23,11 @@ public class RentalService {
     final RentalHistoryRepository rentalHistoryRepository;
     final FacilityFinder facilityFinder;
     final RentalScheduler rentalScheduler;
+    final StudentFinder studentFinder;
+
 
     @Transactional
-    public void create(Member member, CreateRentalRequest createRentalRequest) {
+    public void create(Student student, CreateRentalRequest createRentalRequest) {
         Facility facility = facilityFinder.findById(
             Long.parseLong(createRentalRequest.facilityId()));
 
@@ -30,7 +36,23 @@ public class RentalService {
             createRentalRequest.startTime(),
             createRentalRequest.endTime());
 
-        RentalHistory rentalHistory = createRentalRequest.toEntity(member, facility);
+        RentalHistory rentalHistory = createRentalRequest.toEntity(student, facility);
         rentalHistoryRepository.save(rentalHistory);
+    }
+
+    public List<RentalHistoryResponseDto> getAllRentalHistory() {
+        List<RentalHistory> rentalHistories = rentalHistoryRepository.findAll();
+        return rentalHistories.stream()
+            .map(RentalHistoryResponseDto::from)
+            .toList();
+    }
+
+    public List<RentalHistoryResponseDto> getAllRentalHistoryByStudentId(String studentId) {
+        Student student = studentFinder.findById(studentId);
+        List<RentalHistory> rentalHistories = rentalHistoryRepository.findAllByStudent(
+            student);
+        return rentalHistories.stream()
+            .map(RentalHistoryResponseDto::from)
+            .toList();
     }
 }
