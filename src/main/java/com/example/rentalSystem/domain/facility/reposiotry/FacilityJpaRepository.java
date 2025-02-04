72fcab6fa023959649858d8db30f6e95ad7b280b
0702Yoon <gkstkddbs99@naver.com>
2025-02-04 17:40:34 +0900

    refactor : not import 제거

--- src/main/java/com/example/rentalSystem/domain/facility/reposiotry/FacilityJpaRepository.java
+++ src/main/java/com/example/rentalSystem/domain/facility/reposiotry/FacilityJpaRepository.java
@@ -1,7 +1,6 @@
 package com.example.rentalSystem.domain.facility.reposiotry;
 
 import com.example.rentalSystem.domain.facility.entity.Facility;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FacilityJpaRepository extends JpaRepository<Facility, Long> {
