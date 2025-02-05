5afd114a65a99e7f419c3e285b59b2b29e8bc7e2
0702Yoon <gkstkddbs99@naver.com>
2025-02-05 23:32:14 +0900

    feat : 시설 타입별 시설 조회 api 구현

--- src/main/java/com/example/rentalSystem/domain/facility/reposiotry/FacilityJpaRepository.java
+++ src/main/java/com/example/rentalSystem/domain/facility/reposiotry/FacilityJpaRepository.java
@@ -1,9 +1,12 @@
 package com.example.rentalSystem.domain.facility.reposiotry;
 
 import com.example.rentalSystem.domain.facility.entity.Facility;
+import com.example.rentalSystem.domain.facility.entity.FacilityType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FacilityJpaRepository extends JpaRepository<Facility, Long> {
 
-//  Optional<Facility> findByNameAndLocation(String name, String location);
+    Page<Facility> findByFacilityType(FacilityType instanceByValue, Pageable pageable);
 }
