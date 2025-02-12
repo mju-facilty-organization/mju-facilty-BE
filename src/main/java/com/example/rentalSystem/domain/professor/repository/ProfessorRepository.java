d08233ea18818461bb651bde2dddee1799b9151b
0702Yoon <gkstkddbs99@naver.com>
2025-02-12 13:38:25 +0900

    feat : 교수 entity 추가

---
+++ src/main/java/com/example/rentalSystem/domain/professor/repository/ProfessorRepository.java
@@ -0,0 +1,10 @@
+package com.example.rentalSystem.domain.professor.repository;
+
+import com.example.rentalSystem.domain.professor.entity.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+}
