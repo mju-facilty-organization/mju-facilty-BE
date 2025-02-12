2280af4a67b8ae06c9a454fb0728e7137ee72b2e
0702Yoon <gkstkddbs99@naver.com>
2025-02-12 20:51:08 +0900

    feat : 개발 중 필요한 더미 데이터인 교수, 교직원 넣는 로직 추가

---
+++ src/main/java/com/example/rentalSystem/global/initializer/DataInitializer.java
@@ -0,0 +1,48 @@
+package com.example.rentalSystem.global.initializer;
+
+import com.example.rentalSystem.domain.affiliation.type.AffiliationType;
+import com.example.rentalSystem.domain.member.entity.Role;
+import com.example.rentalSystem.domain.pic.entity.Pic;
+import com.example.rentalSystem.domain.pic.repository.PicRepository;
+import com.example.rentalSystem.domain.professor.entity.Professor;
+import com.example.rentalSystem.domain.professor.repository.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private PicRepository picRepository;
+
+    @Autowired
+    private ProfessorRepository professorRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (picRepository.count() == 0) {
+            Pic pic = Pic.builder()
+                .phoneNumber("010-1111-2222")
+                .password(passwordEncoder.encode("picPassword"))
+                .college(AffiliationType.ICT.getName())
+                .email("pic@a.com")
+                .name("교직원")
+                .role(Role.PIC)
+                .build();
+            picRepository.save(pic);
+        }
+        if (professorRepository.count() == 0) {
+            Professor professor = Professor.builder()
+                .affiliationType(AffiliationType.ICT)
+                .email("professor@e.com")
+                .name("최성운")
+                .build();
+            professorRepository.save(professor);
+        }
+    }
+}
