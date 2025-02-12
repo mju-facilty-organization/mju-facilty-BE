2711f89b45485cbb1fc319f3bad493c53bc43f63
0702Yoon <gkstkddbs99@naver.com>
2025-02-12 14:22:29 +0900

    feat : 교직원 엔터티 추가

---
+++ src/main/java/com/example/rentalSystem/domain/professor/controller/ProfessorController.java
@@ -0,0 +1,13 @@
+package com.example.rentalSystem.domain.professor.controller;
+
+import com.example.rentalSystem.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/professor")
+public class ProfessorController {
+
+}
