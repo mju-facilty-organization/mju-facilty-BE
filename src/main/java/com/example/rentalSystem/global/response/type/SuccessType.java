cf376b5578e5f2a589188c0c5a3e8ca352f70ad5
0702Yoon <gkstkddbs99@naver.com>
2025-02-19 15:02:01 +0900

    refactor : type 클래스들을 패키징 하기

---
+++ src/main/java/com/example/rentalSystem/global/response/type/SuccessType.java
@@ -0,0 +1,17 @@
+package com.example.rentalSystem.global.response.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessType {
+
+    SUCCESS(200, "요청에 성공하였습니다."),
+    CREATED(201, "성공적으로 생성하였습니다."),
+    NO_CONTENT(204, "요청에 성공하였습니다.");
+
+    private final int httpStatusCode;
+    private final String message;
+}
