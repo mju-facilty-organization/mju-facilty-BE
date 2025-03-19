f551f86240576c8e80e0756642c90733f2786470
0702Yoon <gkstkddbs99@naver.com>
2025-03-19 14:04:27 +0900

    [Refactor] EmailService안에 존재했던 Repo 의존성 제거

--- src/main/java/com/example/rentalSystem/domain/email/service/EmailService.java
+++ src/main/java/com/example/rentalSystem/domain/email/service/EmailService.java
@@ -2,11 +2,9 @@ package com.example.rentalSystem.domain.email.service;
 
 import com.example.rentalSystem.domain.email.controller.dto.request.EmailRequest;
 import com.example.rentalSystem.domain.email.controller.dto.response.EmailVerificationResult;
-import com.example.rentalSystem.domain.email.entity.AuthCodeEmail;
 import com.example.rentalSystem.domain.email.implement.MailChecker;
-import com.example.rentalSystem.domain.email.implement.MailLoader;
+import com.example.rentalSystem.domain.email.implement.MailHandler;
 import com.example.rentalSystem.domain.email.implement.MailMaker;
-import com.example.rentalSystem.domain.email.repository.MailRepository;
 import com.example.rentalSystem.global.exception.custom.CustomException;
 import com.example.rentalSystem.global.response.type.ErrorType;
 import java.util.UUID;
@@ -14,35 +12,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final long authCodeExpirationMillis = 1800000;
 
     private final MailChecker mailChecker;
     private final MailMaker mailMaker;
-    private final MailLoader mailLoader;
+    private final MailHandler mailHandler;
     private final JavaMailSender javaMailSender;
-    private final MailRepository mailRepository;
 
     public void checkDuplicatedEmail(EmailRequest emailRequest) {
         mailChecker.checkDuplicateEmail(emailRequest.email());
     }
 
-    @Transactional
     public void sendCodeToEmail(EmailRequest emailRequest) {
         mailChecker.checkDuplicateSend(emailRequest.email());
-        AuthCodeEmail authCodeEmail = mailMaker.makeAuthCodeMail(emailRequest.email());
-        SimpleMailMessage emailForm = mailMaker.createEmailForm(authCodeEmail);
+        String authCode = mailMaker.makeAuthCode();
+        SimpleMailMessage emailForm = mailMaker.createEmailForm(emailRequest.email(), authCode);
         sendEmail(emailForm);
-        mailRepository.save(emailRequest.email(), authCodeEmail.authCode());
+        mailHandler.save(emailRequest.email(), authCode);
     }
-
 
     private void sendEmail(SimpleMailMessage emailForm) {
         try {
@@ -52,14 +44,8 @@ public class EmailService {
         }
     }
 
-    @Scheduled(fixedRate = authCodeExpirationMillis)
-    public void deleteExpiredMail() {
-        mailRepository.deleteExpiredEmail();
-    }
-
-    @Transactional
     public EmailVerificationResult verificationCode(String email, String code) {
-        String saveCode = mailLoader.getAuthCode(email);
+        String saveCode = mailHandler.getAuthCode(email);
         boolean verificationResult = mailChecker.checkAuthCode(saveCode, code);
         return EmailVerificationResult.from(verificationResult);
     }
@@ -67,7 +53,7 @@ public class EmailService {
     @Async("threadPoolTaskExecutor")
     public void sendProfessorRentalConfirm(String email) {
         String token = UUID.randomUUID().toString();
-        mailRepository.saveToken(email, token);
+        mailHandler.saveToken(email, token);
         sendEmail(mailMaker.makeProfessorConfirmEmail(email, token));
     }
 }
