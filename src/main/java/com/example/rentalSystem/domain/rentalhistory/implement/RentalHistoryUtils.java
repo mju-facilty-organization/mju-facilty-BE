50c56bb5d1f23cbab1236dd6031613955dc3fa51
0702Yoon <gkstkddbs99@naver.com>
2025-03-24 22:57:07 +0900

    [Feat] 대여내역에 들어가는 날짜 포맷터 유틸 클래스 구현

---
+++ src/main/java/com/example/rentalSystem/domain/rentalhistory/implement/RentalHistoryUtils.java
@@ -0,0 +1,22 @@
+package com.example.rentalSystem.domain.rentalhistory.implement;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class RentalHistoryUtils {
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(
+        "yyyy년 M월 d일");
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    public static String formatRentalDate(LocalDateTime rentalStartDate) {
+        return rentalStartDate.toLocalDate().format(DATE_FORMATTER);
+    }
+
+    public static String formatRentalTime(LocalDateTime rentalStartDate,
+        LocalDateTime rentalEndDate) {
+        String startTime = rentalStartDate.format(TIME_FORMATTER);
+        String endTime = rentalEndDate.format(TIME_FORMATTER);
+        return startTime + " ~ " + endTime;
+    }
+}
