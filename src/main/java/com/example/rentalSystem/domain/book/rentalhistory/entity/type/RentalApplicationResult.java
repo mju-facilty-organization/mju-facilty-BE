ae23ec2f05ba1e35971fd66e153293769c0e963d
0702Yoon <gkstkddbs99@naver.com>
2025-08-06 17:37:00 +0900

    RentalApplicationResult안에 그 상태일 때의 예약 상태를 갖도록 변경

--- src/main/java/com/example/rentalSystem/domain/book/rentalhistory/entity/type/RentalApplicationResult.java
+++ src/main/java/com/example/rentalSystem/domain/book/rentalhistory/entity/type/RentalApplicationResult.java
@@ -1,5 +1,9 @@
 package com.example.rentalSystem.domain.book.rentalhistory.entity.type;
 
+import static com.example.rentalSystem.domain.book.timetable.TimeStatus.AVAILABLE;
+import static com.example.rentalSystem.domain.book.timetable.TimeStatus.RESERVED;
+
+import com.example.rentalSystem.domain.book.timetable.TimeStatus;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,11 +11,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RentalApplicationResult {
 
-    PIC_PERMITTED("교직원 승인 허가"),
-    PROFESSOR_PERMITTED("교수 승인 허가"),
-    WAITING("대기중"),
-    PIC_DENIED("교직원 승인 거절"),
-    PROFESSOR_DENIED("교수 승인 거절");
+    PIC_PERMITTED("교직원 승인 허가", RESERVED),
+    PROFESSOR_PERMITTED("교수 승인 허가", TimeStatus.WAITING),
+    WAITING("대기중", TimeStatus.WAITING),
+    PIC_DENIED("교직원 승인 거절", AVAILABLE),
+    PROFESSOR_DENIED("교수 승인 거절", AVAILABLE);
 
     private final String description;
+    private final TimeStatus timeStatus;
 }
