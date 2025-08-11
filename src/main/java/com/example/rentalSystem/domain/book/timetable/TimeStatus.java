c4af50e12b633d8996ae2fa94143bc363c879171
0702Yoon <gkstkddbs99@naver.com>
2025-08-11 23:22:22 +0900

    refactor: TimeStatus 현재 예약 삭제

--- src/main/java/com/example/rentalSystem/domain/book/timetable/TimeStatus.java
+++ src/main/java/com/example/rentalSystem/domain/book/timetable/TimeStatus.java
@@ -10,8 +10,7 @@ public enum TimeStatus {
     AVAILABLE("예약가능"),
     UNAVAILABLE("이용불가"),
     RESERVED("예약완료"),
-    WAITING("예약대기"),
-    CURRENT("현재예약");
+    WAITING("예약대기");
     final String description;
 
 }
