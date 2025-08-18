9d8fd3a131f7b9b1b41a7dac836159549ddd69d9
0702Yoon <gkstkddbs99@naver.com>
2025-08-19 00:25:20 +0900

    feat: api한테 넘길 요청을 모으는 util 클래스 구현

---
+++ src/main/java/com/example/rentalSystem/global/util/RequestUtils.java
@@ -0,0 +1,23 @@
+package com.example.rentalSystem.global.util;
+
+import com.example.rentalSystem.domain.facility.dto.FacilityDto;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RequestUtils {
+
+    public static String merge(String question, String extractPrompt) {
+        return question + System.lineSeparator() + extractPrompt;
+    }
+
+    public static String createFacilityDataList(List<FacilityDto> facilityListBy) {
+        return facilityListBy.stream()
+            .map(facility -> String.format(
+                "시설 타입: %s, 시설 번호: %s, 수용 인원: %d명",
+                facility.facilityType(),
+                facility.facilityNumber(),
+                facility.capacity()
+            ))
+            .collect(Collectors.joining("\n"));
+    }
+}
