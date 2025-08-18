9ab5b6a6ee06308a90e7ab1fc1617e81e96cdc36
0702Yoon <gkstkddbs99@naver.com>
2025-08-19 00:27:27 +0900

    feat: 가능한 소속을 포함한 모든 시설 조회하는 로직 구현

--- src/main/java/com/example/rentalSystem/domain/facility/reposiotry/FacilityJpaRepository.java
+++ src/main/java/com/example/rentalSystem/domain/facility/reposiotry/FacilityJpaRepository.java
@@ -2,11 +2,17 @@ package com.example.rentalSystem.domain.facility.reposiotry;
 
 import com.example.rentalSystem.domain.facility.entity.Facility;
 import com.example.rentalSystem.domain.facility.entity.type.FacilityType;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FacilityJpaRepository extends JpaRepository<Facility, Long> {
 
     Page<Facility> findByFacilityType(FacilityType instanceByValue, Pageable pageable);
+
+    @Query(value = "SELECT * FROM facility f WHERE f.allowed_boundary LIKE CONCAT('%\"', :affiliationTypeName, '\"%')", nativeQuery = true)
+    List<Facility> getFacilityListByAffiliationType(@Param("affiliationTypeName") String affiliationTypeName);
 }
