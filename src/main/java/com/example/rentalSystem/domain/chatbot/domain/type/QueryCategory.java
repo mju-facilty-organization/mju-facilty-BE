d420b76eaf46bf620b2e4bd540c5428cb9f53f17
0702Yoon <gkstkddbs99@naver.com>
2025-08-19 00:25:29 +0900

    feat: 챗봇 대화 카테고리 추가

---
+++ src/main/java/com/example/rentalSystem/domain/chatbot/domain/type/QueryCategory.java
@@ -0,0 +1,28 @@
+package com.example.rentalSystem.domain.chatbot.domain.type;
+
+import static com.example.rentalSystem.domain.chatbot.domain.type.PromptConstant.DEPARTMENTAL_ASK_PROMPT;
+import static com.example.rentalSystem.domain.chatbot.domain.type.PromptConstant.DEPARTMENTAL_EXTRACT_PROMPT;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum QueryCategory {
+    // 과별 강의실 (전공 강의실)
+    DEPARTMENTAL_FACILITY("과별 강의실", DEPARTMENTAL_EXTRACT_PROMPT, DEPARTMENTAL_ASK_PROMPT),
+
+    // 사용자가 소속된 과에 따라 필터링된 강의실
+    AVAILABLE_FACILITY("내가 빌릴 수 있는 강의실", null, null),
+
+    // 모든 사용자가 이용할 수 있는 공용 강의실
+    GENERAL_FACILITY("전체 강의실", null, null),
+
+    // 특별한 장비가 있는 강의실
+    SPECIAL_PURPOSE_ROOM("특별 강의실", null, null);
+
+    private final String description;
+    private final String extractPrompt;
+    private final String askPrompt;
+
+}
