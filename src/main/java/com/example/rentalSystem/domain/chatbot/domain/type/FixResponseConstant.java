963bb74736c8ac62bd84760b1ddde562907c9813
0702Yoon <gkstkddbs99@naver.com>
2025-08-19 16:10:28 +0900

    feat: 로그인이 필요한 챗봇 서비스에서 로그인 요청 메시지 추가

--- src/main/java/com/example/rentalSystem/domain/chatbot/domain/type/FixResponseConstant.java
+++ src/main/java/com/example/rentalSystem/domain/chatbot/domain/type/FixResponseConstant.java
@@ -6,4 +6,8 @@ public class FixResponseConstant {
         죄송합니다. 제공하신 학부/전공에 대한 정보를 찾을 수 없습니다. \n 
         혹시 학과 이름이 정확한지 다시 확인해 주시거나, 다른 키워드로 검색해 주시겠어요?
         """;
+    public static final String LOGIN_REQUIRED_MESSAGE = """
+        해당 질문은 로그인을 하셔야 응답이 가능해요!\n
+        로그인이 힘드시면, 학부/과별 카테고리를 선택해서 질문을 주세요!
+        """;
 }
