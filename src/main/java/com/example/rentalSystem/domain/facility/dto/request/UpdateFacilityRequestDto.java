a9b617aadc8773a70501694d13f46681560c0e9d
김윤진(Kim YunJin) <yunjinkim25@mju.ac.kr>
2025-08-20 04:18:30 +0900

    feat: UpdateFacilityRequestDto 날짜 받는 형태 HH:mm으로 고정

--- src/main/java/com/example/rentalSystem/domain/facility/dto/request/UpdateFacilityRequestDto.java
+++ src/main/java/com/example/rentalSystem/domain/facility/dto/request/UpdateFacilityRequestDto.java
@@ -2,21 +2,26 @@ package com.example.rentalSystem.domain.facility.dto.request;
 
 import com.example.rentalSystem.domain.member.base.entity.type.AffiliationType;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalTime;
 import java.util.List;
 
 public record UpdateFacilityRequestDto(
-        String facilityType,
-        String facilityNumber,
-        List<String> supportFacilities,
-        java.time.LocalTime startTime,
-        java.time.LocalTime endTime,
-        Long capacity,
-        Boolean isAvailable,
-        List<AffiliationType> allowedBoundary,
-        List<String> addFileNames,
-        List<String> removeKeys,
-        List<String> newOrder,
-        Boolean hardDelete
+    String facilityType,
+    String facilityNumber,
+    List<String> supportFacilities,
+    @NotNull @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    LocalTime startTime,
+    @NotNull @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    LocalTime endTime,
+    Long capacity,
+    Boolean isAvailable,
+    List<AffiliationType> allowedBoundary,
+    List<String> addFileNames,
+    List<String> removeKeys,
+    List<String> newOrder,
+    Boolean hardDelete
 
 ) {
 
