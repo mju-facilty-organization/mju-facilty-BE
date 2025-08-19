5b957922a45e426e1f8b1a3aaa8bcac6b6bf9e35
0702Yoon <gkstkddbs99@naver.com>
2025-08-19 17:04:11 +0900

    refactor: enum 변수 변경에 따른 request example 변경

--- src/main/java/com/example/rentalSystem/infrastructure/adapter/openai/dto/ChatBotRequest.java
+++ src/main/java/com/example/rentalSystem/infrastructure/adapter/openai/dto/ChatBotRequest.java
@@ -4,7 +4,7 @@ import com.example.rentalSystem.domain.chatbot.domain.type.QueryCategory;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ChatBotRequest(
-    @Schema(description = "질문의 종류", example = "DEPARTMENTAL_FACILITY,AVAILABLE_FACILITY,GENERAL_FACILITY,SPECIAL_PURPOSE_ROOM")
+    @Schema(description = "질문의 종류", example = "DEPARTMENTAL_FACILITY,USER_FACILITY,GENERAL_FACILITY,SPECIAL_PURPOSE_ROOM")
     QueryCategory queryCategory,
     String question
 ) {
