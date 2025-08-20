44968c4e7721cf30ec7a477253aa5e4c245287dd
김윤진(Kim YunJin) <yunjinkim25@mju.ac.kr>
2025-08-20 14:15:04 +0900

    refactor: 사진 등록

--- src/main/java/com/example/rentalSystem/domain/facility/reposiotry/FacilityJpaRepository.java
+++ src/main/java/com/example/rentalSystem/domain/facility/reposiotry/FacilityJpaRepository.java
@@ -11,30 +11,32 @@ import org.springframework.data.repository.query.Param;
 
 public interface FacilityJpaRepository extends JpaRepository<Facility, Long> {
 
-    @Query(
-        value = """
-            SELECT *
-            FROM facility f
-            WHERE (:type IS NULL OR f.facility_type = :type)
-            ORDER BY
-              CHAR_LENGTH(SUBSTRING(f.facility_number, 2)) ASC,
-              SUBSTRING(f.facility_number, 2) ASC
-            """,
-        countQuery = """
-            SELECT COUNT(*)
-            FROM facility f
-            WHERE (:type IS NULL OR f.facility_type = :type)
-            """,
-        nativeQuery = true
-    )
-    Page<Facility> findByFacilityType(@Param("type") String type, Pageable pageable);
+  @Query(
+      value = """
+          SELECT *
+          FROM facility f
+          WHERE (:type IS NULL OR f.facility_type = :type)
+          ORDER BY
+            CHAR_LENGTH(SUBSTRING(f.facility_number, 2)) ASC,
+            SUBSTRING(f.facility_number, 2) ASC
+          """,
+      countQuery = """
+          SELECT COUNT(*)
+          FROM facility f
+          WHERE (:type IS NULL OR f.facility_type = :type)
+          """,
+      nativeQuery = true
+  )
+  Page<Facility> findByFacilityType(@Param("type") String type, Pageable pageable);
 
-    Optional<Facility> findByFacilityNumber(String facilityNumber);
+  Optional<Facility> findByFacilityNumber(String facilityNumber);
 
-    @Query(
-        value = "SELECT * FROM facility f " +
-            "WHERE f.allowed_boundary LIKE CONCAT('%\"', :affiliationTypeName, '\"%')",
-        nativeQuery = true
-    )
-    List<Facility> getFacilityListByAffiliationType(@Param("affiliationTypeName") String affiliationTypeName);
+  @Query(
+      value = "SELECT * FROM facility f " +
+          "WHERE f.allowed_boundary LIKE CONCAT('%\"', :affiliationTypeName, '\"%')",
+      nativeQuery = true
+  )
+  List<Facility> getFacilityListByAffiliationType(@Param("affiliationTypeName") String affiliationTypeName);
+
+  boolean existsByFacilityNumberAndIdNot(String facilityNumber, Long id);
 }
